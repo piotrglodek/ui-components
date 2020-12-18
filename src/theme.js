6170b90cad0bd05b9756cdfbbf80e2b278bbd5bf
Piotr Glodek <pioglodek@gmail.com>
2020-12-18 15:01:02 +0100

    Create theme

---
+++ src/theme.js
@@ -0,0 +1,51 @@
+import {
+  ThemeProvider as StyledThemeProvider,
+  createGlobalStyle,
+} from 'styled-components';
+
+const theme = {
+  color: {
+    primary: '#40407a',
+    darkPrimary: '#2c2c54',
+    secondary: '#ffb142',
+    darkSecondary: '#eea031',
+    white: '#ffffff',
+    gray: '#e0e0e0',
+    darkGray: '#c0c0c0',
+    black: '#4b4b4b',
+    darkBlack: '#3d3d3d',
+  },
+  fontSize: {
+    button: '1.4rem',
+  },
+  fontWeight: {
+    normal: '400',
+    semiBold: '600',
+  },
+  radius: '.4rem',
+};
+
+const GlobalStyles = createGlobalStyle`
+    *,*::after,*::before{
+        box-sizing: border-box;
+    }
+
+    html{
+        font-size:62.5%;
+    }
+
+    body{
+        margin:0;
+        font-size:1.6rem;
+        font-family: 'Inter', sans-serif;
+    }
+`;
+
+export const ThemeProvider = ({ children }) => {
+  return (
+    <StyledThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </StyledThemeProvider>
+  );
+};
